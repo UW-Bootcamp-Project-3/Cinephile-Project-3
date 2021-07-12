c733a413a8a0737bd8a5de44199ee7c9850ce78f
Abbey Schueller <schuellerabbey@gmail.com>
2021-07-11 19:28:10 -0500

    more updates

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 function Login() {
+
+  const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const email = document.querySelector('#exampleInputEmail1').value.trim();
+    const password = document.querySelector('#exampleInputPassword1').value.trim();
+  
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
   return (
     <div className="container">
       <form>
         <div className="form-group">
-          <label for="exampleInputEmail1">
+          <label htmlFor="exampleInputEmail1">
             <strong>Email address</strong>
           </label>
           <input
@@ -19,7 +43,7 @@ function Login() {
           <small id="emailHelp" className="form-text text-muted"></small>
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1">
+          <label htmlFor="exampleInputPassword1">
             <strong>Password</strong>
           </label>
           <input
@@ -31,7 +55,7 @@ function Login() {
           />
         </div>
 
-        <button id="login-form" type="button" className="btn btn-warning">
+        <button onClick={loginFormHandler} id="login-form" type="button" className="btn btn-warning">
           Login
         </button>
       </form>
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,29 @@
 import React from "react";
 
 function Signup() {
+
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
   return (
     <div
       className="base-container"
@@ -47,6 +70,7 @@ function Signup() {
               type="button"
               id="signup-form"
               className="btn btn-warning"
+              onClick={signupFormHandler}
               style={{ margin: "20px" }}
             >
               Register
