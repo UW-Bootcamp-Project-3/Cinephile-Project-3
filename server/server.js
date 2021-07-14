04b2c6d767c3441e18fb392951c11be054bc1a9e
Abbey Schueller <schuellerabbey@gmail.com>
2021-07-13 22:09:42 -0500

    updated file structure

--- server/server.js
+++ server/server.js
@@ -29,20 +29,17 @@ const sess = {
 app.use(session(sess));
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static("public"));
 
+
+// Define middleware here
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
 }
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // Add routes, both API and view
 app.use(routes);
 
