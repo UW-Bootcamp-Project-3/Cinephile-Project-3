f13db0289d25024ea9bd975c9ce664720347278f
Abbey Schueller <schuellerabbey@gmail.com>
2021-07-19 21:46:41 -0700

    login logout

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Contact from "./components/contact/Contact";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import NavigationWithAuth from "./components/NavigationWithAuth";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
@@ -14,16 +15,24 @@ import Movies from "../src/components/Movies";
 import Profile from "../src/components/Profile";
 import ParticleBackground from "./components/ParticleBackground";
 
+function AuthBtn(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <NavigationWithAuth />;
+  }
+  return <Navigation />;
+};
+
 
 function App() {
+  const {isLoggedIn} = this.state;
   return (
-
     
     <div>  
       <img src= {Banner} alt="Banner" className="Banner"/>
     <Router>
       <div>
-        <Navigation />
+        <AuthBtn isLoggedIn={isLoggedIn} />
         <Route exact path="/" component={Home} />
         <Route exact path="/movies" component={Movies} />
         <Route exact path="/login" component={Login} />
---
+++ client/src/components/NavigationWithAuth.js
@@ -0,0 +1,86 @@
+import { Alert } from "bootstrap";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-warning px-2 font-weight-bold ">
+      <button
+        className="navbar-toggler px-2"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        // aria-controls="navbarSupportedContent"
+        // aria-expanded="false"
+        // aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item dropdown px-2">
+            <Link
+              style={{
+                color: "black",
+                textDecoration: "none",
+                backgroundcolor: "black",
+              }}
+              to="/home"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item dropdown px-2">
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/movies"
+            >
+              Movies
+            </Link>
+          </li>
+          <li className="nav-item dropdown px-2">
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/Profile"
+            >
+              Profile
+            </Link>
+          </li>
+          <li className="nav-item dropdown px-2">
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/logout"
+            >
+              Logout
+            </Link>
+          </li>
+          <li className="nav-item dropdown px-2">
+            <Link
+              style={{ color: "black", textDecoration: "none" }}
+              to="/contact"
+            >
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0 float-right"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+export default Navigation;
