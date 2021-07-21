0f372b1d9b5b44ecdabb339935e388feb2da98cf
Abbey Schueller <schuellerabbey@gmail.com>
2021-07-21 16:49:52 -0700

    event counter update

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -6,34 +6,44 @@ function Events(){
     document.querySelector("#rsvp1").style.display = "none";
     document.querySelector("#remove1").style.display = "block";
     document.querySelector("#chat1").style.display = "block";
+    var i = 0;
+    document.querySelector('#number1').innerHTML = ++i;
   };
   function attending2(){
     document.querySelector("#rsvp2").style.display = "none";
     document.querySelector("#remove2").style.display = "block";
     document.querySelector("#chat2").style.display = "block";
+    var i = 0;
+    document.querySelector('#number2').innerHTML = ++i;
   };
   function attending3(){
     document.querySelector("#rsvp3").style.display = "none";
     document.querySelector("#remove3").style.display = "block";
     document.querySelector("#chat3").style.display = "block";
+    var i = 0;
+    document.querySelector('#number3').innerHTML = ++i;
   };
   function decline1(){
     document.querySelector("#rsvp1").style.display = "block";
     document.querySelector("#remove1").style.display = "none";
     document.querySelector("#chat1").style.display = "none";
+    var i = document.querySelector('#number1').innerHTML;
+    document.querySelector('#number1').innerHTML = --i;
   };
   function decline2(){
     document.querySelector("#rsvp2").style.display = "block";
     document.querySelector("#remove2").style.display = "none";
     document.querySelector("#chat2").style.display = "none";
+    var i = document.querySelector('#number2').innerHTML;
+    document.querySelector('#number2').innerHTML = --i;
   };
   function decline3(){
     document.querySelector("#rsvp3").style.display = "block";
     document.querySelector("#remove3").style.display = "none";
     document.querySelector("#chat3").style.display = "none";
+    var i = document.querySelector('#number3').innerHTML;
+    document.querySelector('#number3').innerHTML = --i;
   };
-
-
 
     return(
 <div className="card-deck">
@@ -47,7 +57,7 @@ function Events(){
     </div>
     <div className="count">
     <img alt="icon" src="/images/people-fill.svg" className="bi bi-people-fill"></img>
-    <p className="card-text">0</p>
+    <p id="number1" className="card-text">0</p>
     </div>
     <button id="rsvp1" onClick={attending1} className="btn btn-secondary">RSVP!</button>
     <button id="remove1" onClick={decline1} className="btn btn-outline-dark" style={{display: 'none'}}>I can no longer attend</button>
@@ -66,7 +76,7 @@ function Events(){
     </div>
     <div className="count">
     <img alt="icon" src="/images/people-fill.svg" className="bi bi-people-fill"></img>
-    <p className="card-text">0</p>
+    <p id="number2" className="card-text">0</p>
     </div>
     <button onClick={attending2} id="rsvp2" className="btn btn-secondary">RSVP!</button>
     <button onClick={decline2} id="remove2" className="btn btn-outline-dark" style={{display: 'none'}}>I can no longer attend</button>
@@ -85,10 +95,10 @@ function Events(){
     </div>
     <div className="count">
     <img alt="icon" src="/images/people-fill.svg" className="bi bi-people-fill"></img>
-    <p className="card-text">0</p>
+    <p id="number3" className="card-text">0</p>
     </div>
     <button onClick={attending3} id="rsvp3" className="btn btn-secondary">RSVP!</button>
-    <button onClick={decline2} id="remove3" className="btn btn-outline-dark" style={{display: 'none'}}>I can no longer attend</button>
+    <button onClick={decline3} id="remove3" className="btn btn-outline-dark" style={{display: 'none'}}>I can no longer attend</button>
     <button id="chat3" className="btn btn-warning" style={{display: 'none'}}>Chat with other attendees</button>
     <div className="card-footer">
       <small className="text-muted">Rated R</small>
